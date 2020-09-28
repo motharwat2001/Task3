b8c1e0b49f01a64e5db1d229c78a711244431c06
motharwat2001 <71636898+motharwat2001@users.noreply.github.com>
2020-09-28 13:04:41 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,41 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-int sum(int arr[],int size)
-{ int sum=0;
-  for(int y=0;y<size;y++)
-  {
-    sum+=arr[y];
-  }
-  return sum;
-}
 
 int main()
-{   int n=20;
-    int arr[n],i;
-    printf("please enter the elements of array\n");
-    for(i=0;i<n;i++)
-    {
-      scanf("%d",&arr[i]);
-      for(int j=-1;j<i;j++)
-      {
-        if(arr[j]==arr[i])
-        {
-          printf("this number is duplicated\n");
-          i--;
-          n--;
-          break;
-        }
+{
 
-      }
+    char s[16];	//maximum 16 letter
 
-    }
-    printf("the elements of unique array:- ");
-    for(i=0;i<n;i++)
-    {
-      printf(" %d  ",arr[i]);
-    }
-    printf("\n");
-    printf("sum of unique array is %d",sum(arr,n));
+
+
+		int i =0;
+		while (i<16){
+			s[i] = 0;
+			i = i+1;
+		}
+		scanf("%s", &s);
+		i = 0;
+		int len;
+		while (i<16){
+			if (s[i] == 0){
+				len = i;
+				break;
+			}
+			i = i+1;
+		}
+
+		char reverse[len];
+	i = 0;
+	int j = 2;
+	int x = len;
+	while (i<len){
+		if (!(s[i]>=97 && s[i]<=122)){
+			j = 0;
+			i=16;
+		}
+		reverse[x-1] = s[i];
+		i = i+1;
+		x = x-1;
+	}
+	i = 0;
+
+
+	while (i<len){
+		if (!(s[i]==reverse[i])){
+			j = 3;
+			break;
+		}
+		i = i+1;
+	}
+
+	if (j==0){
+		printf("Wrong Input");
+
+	}
+	else if (j == 3){
+			printf("no");
+	}
+	else{
+		printf("yes");
+	}
 
     return 0;
 }
